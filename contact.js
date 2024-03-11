65af87a67eed37a082fdc87eebe8faabc727d874
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-11 22:02:21 +0530

    added contact links

---
+++ contact.js
@@ -0,0 +1,18 @@
+export default contact = [
+    {
+        icon: "",
+        link: "https://twitter.com/taruuu_bhandari"
+    },
+    {
+        icon: "",
+        link: "https://www.instagram.com/tar._.u/"
+    },
+    {
+        icon: "",
+        link: "https://anilist.co/user/iamtaruuuu/mangalist"
+    },
+    {
+        icon: "",
+        link: "https://github.com/tarun-bhandari"
+    }
+]
